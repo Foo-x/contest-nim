e6675b678e6ba95ee9d32b8e476288fb3894ddda
Foo-x <www.sand.x@gmail.com>
2020-07-24 14:33:20 +0900

    Clean contest template

--- src/aoj_itp/itp1_8_c.nim
+++ src/aoj_itp/itp1_8_c.nim
@@ -1,30 +1,4 @@
-import strformat, macros, algorithm, tables, sets, lists,
-    intsets, critbits, sequtils, strutils, math, times, sugar
-
-template stopwatch(body) = (let t1 = cpuTime(); body; stderr.writeLine "TIME:",
-    (cpuTime() - t1) * 1000, "ms")
-template loop(n: int, body) = (for _ in 0..<n: body)
-template `max=`(x, y) = x = max(x, y)
-template `min=`(x, y) = x = min(x, y)
-
-let readNext = iterator(getsChar: bool = false): string {.closure.} =
-  for s in stdin.readAll.split:
-    if getsChar:
-      for i in 0..<s.len():
-        yield s[i..i]
-    else:
-      yield s
-
-proc read(t: typedesc[string]): string = readNext()
-proc read(t: typedesc[char]): char = readNext(true)[0]
-proc read(t: typedesc[int]): int = readNext().parseInt
-
-macro read(t: typedesc, n: varargs[int]): untyped =
-  var repStr = ""
-  for arg in n:
-    repStr &= &"({arg.repr}).newSeqWith "
-  parseExpr(&"{repStr}read({t})")
-
+include ../ctemplate
 
 when isMainModule:
   var s = stdin.readAll
