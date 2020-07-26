02fd2545547b1d8d2c4822370584b90c1c569644
Foo-x <www.sand.x@gmail.com>
2020-07-26 14:58:01 +0900

    Add bins for convenience

---
+++ bin/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if (( $# != 3 )); then
+    echo "need 3 arguments." 1>&2
+    echo "test.sh site title problem" 1>&2
+    exit 1
+fi
+
+site=$1
+title=$2
+problem=$3
+
+root=$(git rev-parse --show-toplevel)
+cd ${root}/src/${site}/${title}/
+
+nim cpp -r -o:./a.out -d:release --hints:off --verbosity:0 ${title}_${problem}.nim < inputs/${title}_${problem}.txt
