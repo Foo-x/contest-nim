3892c8c728cc55f84da24c3eb1e4743b3ae17162
Foo-x <www.sand.x@gmail.com>
2020-08-09 14:06:21 +0900

    Add sandbox

---
+++ sandbox.nim
@@ -0,0 +1,30 @@
+import strformat, macros, algorithm, tables, sets, lists,
+    intsets, critbits, sequtils, strutils, math, times, sugar, options, deques
+
+const
+  InfInt = 10^12
+  NegInfInt = -10^12
+  MOD = 10^9 + 7
+
+template loop(n: int, body) = (for _ in 0..<n: body)
+template `max=`(x, y) = x = max(x, y)
+template `min=`(x, y) = x = min(x, y)
+
+let readNext = iterator(getsChar: bool = false): string {.closure.} =
+  for s in stdin.readAll.split:
+    if getsChar:
+      for i in 0..<s.len():
+        yield s[i..i]
+    else:
+      yield s
+
+proc read(t: typedesc[string]): string = readNext()
+proc read(t: typedesc[char]): char = readNext(true)[0]
+proc read(t: typedesc[int]): int = readNext().parseInt
+proc read(t: typedesc[float]): float = readNext().parseFloat
+
+macro read(t: typedesc, n: varargs[int]): untyped =
+  var repStr = ""
+  for arg in n:
+    repStr &= &"({arg.repr}).newSeqWith "
+  parseExpr(&"{repStr}read({t})")
