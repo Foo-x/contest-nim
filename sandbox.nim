5c58902ef710cd3702117df90144732b5642ec7f
Foo-x <www.sand.x@gmail.com>
2020-08-13 02:57:46 +0900

    Add mod templates

--- sandbox.nim
+++ sandbox.nim
@@ -10,6 +10,9 @@ const
 template loop(n: int, body) = (for _ in 0..<n: body)
 template `max=`(x, y) = x = max(x, y)
 template `min=`(x, y) = x = min(x, y)
+template `mod=`(x, y) = x = x mod y
+template `modSum=`(x, y, m = MOD) = x = (x + y) mod m
+template `modProd=`(x, y, m = MOD) = x = (x * y) mod m
 
 let readNext = iterator(getsChar: bool = false): string {.closure.} =
   for s in stdin.readAll.split:
